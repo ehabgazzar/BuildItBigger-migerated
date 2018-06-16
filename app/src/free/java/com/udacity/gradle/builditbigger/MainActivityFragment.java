d3dc946813fd1dd3b51d82689a06df6c4a1dab0e
ehabgazzar <ehab.elgazzar@acu.edu.eg>
2018-06-16 04:52:41 +0200

    migration

--- app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/free/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -9,11 +9,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
 import com.udacity.gradle.builditbigger.R;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.eh.myapplication.backend.myApi.MyApi;
+
 import com.example.jokeviewer.joke_viewer;
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
@@ -24,75 +25,6 @@ import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
 import com.google.api.client.googleapis.services.GoogleClientRequestInitializer;
 
 import java.io.IOException;
-
-
-/**
- * A simple {@link Fragment} subclass.
- */
-
-class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
-    private static MyApi myApiService = null;
-    private Context context;
-    ProgressDialog pd;
-
-    public EndpointsAsyncTask(Context c) {
-        context=c;
-    }
-
-    @Override
-    protected void onPreExecute()
-    {
-        super.onPreExecute();
-        pd= new ProgressDialog(context);
-        pd.setMessage("loading");
-        pd.show();
-    }
-
-    @Override
-    protected String doInBackground(Context ... params) {
-        if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
-            // end options for devappserver
-
-            myApiService = builder.build();
-        }
-
-        context = params[0];
-
-
-        try {
-            return myApiService.sayHi().execute().getData();
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        pd.dismiss();
-
-        if(!result.contains("connect timed out")) {
-            Intent myIntent = new Intent(context, joke_viewer.class);
-            myIntent.putExtra("joke", result);
-            context.startActivity(myIntent);
-        }
-        else
-        {
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-        }
-    }
-}
 
 
 public class MainActivityFragment extends Fragment {
--- app/src/pro/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
+++ app/src/pro/java/com/udacity/gradle/builditbigger/MainActivityFragment.java
@@ -26,69 +26,6 @@ import java.io.IOException;
 /**
  * Created by Eh on 9/3/2017.
  */
-class EndpointsAsyncTask extends AsyncTask<Context, Void, String> {
-    private static MyApi myApiService = null;
-    private Context context;
-    ProgressDialog pd;
-
-    public EndpointsAsyncTask(Context c) {
-        context=c;
-    }
-
-    @Override
-    protected void onPreExecute()
-    {
-        super.onPreExecute();
-        pd= new ProgressDialog(context);
-        pd.setMessage("loading");
-        pd.show();
-    }
-
-    @Override
-    protected String doInBackground(Context ... params) {
-        if(myApiService == null) {  // Only do this once
-            MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
-                    new AndroidJsonFactory(), null)
-                    // options for running against local devappserver
-                    // - 10.0.2.2 is localhost's IP address in Android emulator
-                    // - turn off compression when running against local devappserver
-                    .setRootUrl("http://10.0.2.2:8080/_ah/api/")
-                    .setGoogleClientRequestInitializer(new GoogleClientRequestInitializer() {
-                        @Override
-                        public void initialize(AbstractGoogleClientRequest<?> abstractGoogleClientRequest) throws IOException {
-                            abstractGoogleClientRequest.setDisableGZipContent(true);
-                        }
-                    });
-            // end options for devappserver
-
-            myApiService = builder.build();
-        }
-
-        context = params[0];
-
-
-        try {
-            return myApiService.sayHi().execute().getData();
-        } catch (IOException e) {
-            return e.getMessage();
-        }
-    }
-
-    @Override
-    protected void onPostExecute(String result) {
-        pd.dismiss();
-
-        if(!result.contains("connect timed out")) {
-            Intent myIntent = new Intent(context, joke_viewer.class);
-            myIntent.putExtra("joke", result);
-            context.startActivity(myIntent);
-        }
-        else
-        {
-            Toast.makeText(context, result, Toast.LENGTH_LONG).show();
-        }
-    }
-}
 public class MainActivityFragment extends Fragment {
     Button button;
 
